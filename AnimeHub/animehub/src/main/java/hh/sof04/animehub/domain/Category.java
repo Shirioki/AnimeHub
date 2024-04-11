8c9f2aff8c82d5226146fd016a7445116bfead38
Ponki Phan <ponkiphan@gmail.com>
2024-04-11 16:57:42 +0300

    Category and Repository done

--- AnimeHub/animehub/src/main/java/hh/sof04/animehub/domain/Category.java
+++ AnimeHub/animehub/src/main/java/hh/sof04/animehub/domain/Category.java
@@ -1,5 +1,58 @@
 package hh.sof04.animehub.domain;
 
-public class Category {
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoryId;
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private List<Anime> animes;
+
+    public Category() {
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Anime> getAnimes() {
+        return animes;
+    }
+
+    public void setAnimes(List<Anime> animes) {
+        this.animes = animes;
+    }
+
+    @Override
+    public String toString(){
+        return "[categoryId=]" + categoryId + ",name=" + name + "]";
+    }
+    
 }
--- AnimeHub/animehub/src/main/java/hh/sof04/animehub/domain/CategoryRepository.java
+++ AnimeHub/animehub/src/main/java/hh/sof04/animehub/domain/CategoryRepository.java
@@ -1,5 +1,8 @@
 package hh.sof04.animehub.domain;
 
-public class CategoryRepository {
+import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
+public interface CategoryRepository extends CrudRepository<Category, Long>{
+    List<Category> findByName(String name);
 }
