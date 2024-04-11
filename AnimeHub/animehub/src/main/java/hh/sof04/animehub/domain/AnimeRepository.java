371ea711d929348852fbd90da3d62d29271f4ff9
Ponki Phan <ponkiphan@gmail.com>
2024-04-11 16:49:15 +0300

    AnimeRepository done

--- AnimeHub/animehub/src/main/java/hh/sof04/animehub/domain/AnimeRepository.java
+++ AnimeHub/animehub/src/main/java/hh/sof04/animehub/domain/AnimeRepository.java
@@ -1,5 +1,8 @@
 package hh.sof04.animehub.domain;
 
-public class AnimeRepository {
+import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
+public interface AnimeRepository extends CrudRepository<Anime, Long> {
+    List<Anime> findByTitle(String title);
 }
