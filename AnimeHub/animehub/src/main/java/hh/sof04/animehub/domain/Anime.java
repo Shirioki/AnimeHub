40f758eef8aa74511e9ada4c1f4fe5f3a0caf9ad
Ponki Phan <ponkiphan@gmail.com>
2024-04-11 16:44:50 +0300

    Added JPA dependency and Anime.java code added

--- AnimeHub/animehub/src/main/java/hh/sof04/animehub/domain/Anime.java
+++ AnimeHub/animehub/src/main/java/hh/sof04/animehub/domain/Anime.java
@@ -1,5 +1,75 @@
 package hh.sof04.animehub.domain;
 
-public class Anime {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
+@Entity
+public class Anime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String episodes;
+    private double score;
+
+    @ManyToOne
+    @JoinColumn(name = "categoryId")
+    private Category category;
+
+    public Anime(String title, String episodes, double score, Category category) {
+        this.title = title;
+        this.episodes = episodes;
+        this.score = score;
+        this.category = category;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getEpisodes() {
+        return episodes;
+    }
+
+    public void setEpisodes(String episodes) {
+        this.episodes = episodes;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+    
+    @Override
+    public String toString() {
+        return "Anime [id=" + id + ", title=" + title + ", episodes=" + episodes + ", score=" + score + ", category=" + category + "]";
+    }
 }
