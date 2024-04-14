4dd47be508f3eb718a8a50cc0b22beef42c625dd
Ponki Phan <ponkiphan@gmail.com>
2024-04-14 23:10:06 +0300

    RestController created

---
+++ AnimeHub/animehub/src/main/java/hh/sof04/animehub/web/RestAnimeController.java
@@ -0,0 +1,31 @@
+package hh.sof04.animehub.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import hh.sof04.animehub.domain.AnimeRepository;
+import hh.sof04.animehub.domain.CategoryRepository;
+import hh.sof04.animehub.domain.Anime;
+import hh.sof04.animehub.domain.Category;
+
+@RestController
+@RequestMapping("/api")
+public class RestAnimeController {
+
+    @Autowired
+    private AnimeRepository animeRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @GetMapping("/animes")
+     public Iterable<Anime> getAnimes(){
+        return animeRepository.findAll();
+     }
+
+    @GetMapping("/categories")
+     public Iterable<Category> getCategories(){
+        return categoryRepository.findAll();
+     }
+
+}
