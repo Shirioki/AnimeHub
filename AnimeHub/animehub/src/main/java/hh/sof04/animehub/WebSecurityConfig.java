48b230f745cbf613f22845e56e6be136bc8fcf4d
Ponki Phan <ponkiphan@gmail.com>
2024-04-15 02:11:48 +0300

    Spring security added

---
+++ AnimeHub/animehub/src/main/java/hh/sof04/animehub/WebSecurityConfig.java
@@ -0,0 +1,50 @@
+package hh.sof04.animehub;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+//import static method antMatcher
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+
+@Configuration
+@EnableMethodSecurity(securedEnabled = true)
+public class WebSecurityConfig{
+    @Autowired
+    private UserDetailsService userDetailsService;	
+	
+    // using lambda
+    
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+        .authorizeHttpRequests(authorize -> authorize
+        	.requestMatchers(antMatcher("/css/**")).permitAll() // Enable css when logged out
+        	.requestMatchers(antMatcher("/signup")).permitAll()
+        	.requestMatchers(antMatcher("/saveuser")).permitAll()		
+        	.anyRequest().authenticated()
+       )
+      .formLogin( formlogin -> formlogin
+          .loginPage("/login")
+          .defaultSuccessUrl("/studentlist", true)
+          .permitAll()
+      )
+      .logout(logout -> logout
+          .permitAll()
+      );
+     return http.build();   
+    }
+    
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+}
